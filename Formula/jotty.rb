df2e389ab5abf765a5a45e77ab8597311913b00a
Yoshimasa Kuroyama <66538624+ys319@users.noreply.github.com>
2025-10-05 04:36:57 +0900

    update jotty.rb

--- Formula/jotty.rb
+++ Formula/jotty.rb
@@ -2,7 +2,7 @@ class Jotty < Formula
   desc "On-device transcription utility for audio files"
   homepage "https://github.com/ys319/jotty"
   version "0.1.0"
-  url "https://github.com/ys319/jotty/releases/download/v0.1.0/jotty-v0.1.0-macos-arm64.zip"
+  url "https://github.com/ys319/jotty/releases/download/v#{version}/jotty-#{version}-macos-arm64.tar.gz"
   sha256 "bec56161e8817488037dbd563576849abf9cf93da6aa0729199e2940fc78faee"
   license "MIT"
   depends_on :macos
